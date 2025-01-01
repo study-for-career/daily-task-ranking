d03dd0f5d6fe7d3e8afcc91d543d262de7c29079
Md. Zahidul Islam <mdzahidulislam.bera@gmail.com>
2025-01-01 20:30:44 +0600

    Rajab month Added

--- ranking.js
+++ ranking.js
@@ -1,8 +1,8 @@
 
 const date30 = [
-    0, '১','২','৩','৪','৫','৬','৭','৮','৯','১০',
-    '১১','১২','১৩','১৪','১৫','১৬','১৭','১৮','১৯','২০',
-    '২১','২২','২৩','২৪','২৫','২৬','২৭','২৮','২৯','৩০'
+    "০", '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯', '১০',
+    '১১', '১২', '১৩', '১৪', '১৫', '১৬', '১৭', '১৮', '১৯', '২০',
+    '২১', '২২', '২৩', '২৪', '২৫', '২৬', '২৭', '২৮', '২৯', '৩০'
 ]
 
 const dateContainer = document.getElementById('date');
@@ -11,15 +11,15 @@ const dateContainer = document.getElementById('date');
 const date = new Date();
 const day = date.getUTCDate();
 
-const arabicDate = date30[day - 3];
+const arabicDate = date30[day - 1];
 
 dateContainer.innerText = arabicDate;
 
-setInterval(()=>{
+setInterval(() => {
     dateContainer.classList.remove('text-red-600');
 }, 1000)
 
-setInterval(()=>{
+setInterval(() => {
     dateContainer.classList.add('text-red-600');
 }, 2000)
 
@@ -33,41 +33,40 @@ let avgArr = [];
 
 fetch('https://script.googleusercontent.com/macros/echo?user_content_key=i7Onz-aJ1_xNNp5JP9SrNaRqDuKGyFZztDkOdGUDnhMuCnv5JhvfCHRCJWHkCJBaOMfIFDf5cviCMfgfKpQuaas8jF1yjwhAm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnODCNqygfkkrHh1mTBhDHqZkjsk-mYo9vdOzeHj5AGy9Y0O3FbcTYeji5s0Ca5hiR59U0_W4_deaHD1dAQ-gvCn5Uj8bW7vOxg&lib=MTSpZjyWTfXf996pC7Nkk3Zj8bPnJHMmz')
     .then(response => response.json())
-    .then(data => 
-    {
-        
+    .then(data => {
+
         // console.log(data[2])
-        let singleUserData = data.map(function (singleUser){
+        let singleUserData = data.map(function (singleUser) {
 
-        const userScore = singleUser[1].split(',');
+            const userScore = singleUser[1].split(',');
 
-        
-        const currentMonthSubmition = userScore.length - singleUser[2];
 
-        // Get scores of the current month
-        const currentMonthScores = userScore.slice(0, currentMonthSubmition);
+            const currentMonthSubmition = userScore.length - singleUser[2];
+
+            // Get scores of the current month
+            const currentMonthScores = userScore.slice(0, currentMonthSubmition);
             // console.log(currentMonthScores)
-        const total = currentMonthScores.reduce((pre, curr)=> {
-            return  pre + parseFloat(curr);
-        }, 0)
-        // console.log(total)
-        const avg = total * 100/(currentMonthScores.length ? currentMonthScores.length * 10 : 1 * 10);
-              console.log(currentMonthSubmition, total, currentMonthScores.length)  
+            const total = currentMonthScores.reduce((pre, curr) => {
+                return pre + parseFloat(curr);
+            }, 0)
+            // console.log(total)
+            const avg = total * 100 / (currentMonthScores.length ? currentMonthScores.length * 10 : 1 * 10);
+            console.log(currentMonthSubmition, total, currentMonthScores.length)
 
-        avgArr.push(
-            {user: singleUser[0], scores: avg, totalSubmit: currentMonthScores.length, preTotalSubmit: singleUser[2]}
-        )
+            avgArr.push(
+                { user: singleUser[0], scores: avg, totalSubmit: currentMonthScores.length, preTotalSubmit: singleUser[2] }
+            )
         })
-        const validScores = avgArr.filter((invalid)=> !isNaN(invalid.scores))
-        
+        const validScores = avgArr.filter((invalid) => !isNaN(invalid.scores))
+
         // change validScores to avgArr to show all participants
         const rank = avgArr.sort((a, b) => b.scores - a.scores);
         // console.log(rank)
-        const finalRanking = rank.map((singleRank, index)=> {
-        
-        const tr = document.createElement('tr');
-        tr.className = 'bg-blue-100';
-        tr.innerHTML = `
+        const finalRanking = rank.map((singleRank, index) => {
+
+            const tr = document.createElement('tr');
+            tr.className = 'bg-blue-100';
+            tr.innerHTML = `
         <tr>
                   <th>${index + 1}</th>
                   <td>${singleRank.user}</td>
@@ -76,15 +75,15 @@ fetch('https://script.googleusercontent.com/macros/echo?user_content_key=i7Onz-a
         </tr>
 
         `
-        rankingArea.appendChild(tr)
+            rankingArea.appendChild(tr)
         })
 
         loader.classList.add('hidden');
         rankingDiv.classList.remove('hidden');
-        
+
     }
-              
-        
+
+
     );
 
 
